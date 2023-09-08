181840ed9ca5a2f284d9d309d2eef80f182d79e2
Imoushiki <imoushiki@example.com>
2023-09-08 15:57:18 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@ int main(void)
 
 {
 int i;
-char j;
 
 for (i = 0; i < 0; i++)
-putchar((i % 10) + '0');
-for (j = 'a'; j <= 'f'; j++)
-putchar(j);
+{
+putchar(i + '0');
+if (i == 9)
+	continue;
+putchar (',');
+putchar (' ');
+}
 putchar('\n');
 
 return (0);
