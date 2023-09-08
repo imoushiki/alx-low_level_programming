4923c643eb8bfd9850dd75ebd713babd791bac41
Imoushiki <imoushiki@example.com>
2023-09-08 15:47:15 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+
+{
+int i;
+char j;
+
+for (i = 0; i < 0; i++)
+putchar(i + '0');
+for (j = 'a'; j <= 'f'; j++)
+putchar(j);
+putchar('\n');
+
+return (0);
+
+}
