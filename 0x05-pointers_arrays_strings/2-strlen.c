a5a2f3faf960005b9e8ff8e0f832762ad8fa26a7
Imoushiki <imoushiki@example.com>
2023-09-18 14:59:19 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _strlen - returns length of string
+ * @s: string pointer to be checked
+ * Return: return length of string
+ */
+int _strlen(char *s)
+{
+int c = 0;
+while (*(s + c) != 0)
+{
+c++;
+}
+return (c);
+}
